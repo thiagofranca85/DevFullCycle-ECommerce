db638232a81da5295060b81de95a23214daa0f31
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2024-01-30 23:02:11 -0300

    Add 1st files for GOAPI

---
+++ GOAPI/internal/database/category_db.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/thiagofranca85/DevFullCycle-ECommerce/goapi/internal/entity"
+
+type CategoryDB struct {
+	db *sql.DB
+}
+
+func NewCategoryDB(db *sql.DB) *CategoryDB {
+	return &CategoryDB{db: db}
+}
+
+func (cd *CategoryDB) GetCategories() ([]*entity.Category, error) {
+	rows, err := cd.db.Query("SELECT id, name FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+}
